5114985dbbaef9d2082132a61b14d4f7b0b381b3
Cédric <cedric.leprohon@yahoo.fr>
2019-02-04 14:57:50 +0100

    caroussel

--- Project/index.php
+++ Project/index.php
@@ -29,6 +29,39 @@
     </nav>
     <!-- Header = Story 1 --->
     <section class="container-fluid">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item">
+      <img class="d-block w-100" src="http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg">
+      <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+    </div>
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://www.adweek.com/files/2015_May/iStock-Unfinished-Business-6.jpg" data-holder-rendered="true">
+      <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://44u8552epjw3rivfs1yfikr1-wpengine.netdna-ssl.com/wp-content/uploads/2017/11/young-man-2939344_1280.jpg">
+      <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     </section>
     <!-- Story 2 --->
     <section class="container-fluid">
