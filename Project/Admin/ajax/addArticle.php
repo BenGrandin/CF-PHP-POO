0f213618ff2cab608b5f139451225b790bbdfc0a
Cédric <cedric.leprohon@yahoo.fr>
2019-02-05 14:51:48 +0100

    ajax

---
+++ Project/Admin/ajax/addArticle.php
@@ -0,0 +1,55 @@
+		<!-- BEGIN CSS for this page --> 
+		<link rel="stylesheet" href="assets/plugins/trumbowyg/ui/trumbowyg.min.css"> 
+		<!-- END CSS for this page --> 
+
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">						 
+						<div class="card mb-3"> 
+							<div class="card-header"> 
+								<h3><i class="fa fa-file-o"></i> Ajouter un article</h3> 
+								Et faire en sorte que ca marche. 
+							</div> 
+							<div class="card-body"> 
+								 
+ 
+								<form class="demo-form" action="#" novalidate=""> 
+									  <div class="form-section current"> 
+										<label for="firstname">Nom de l'article : </label> 
+										<input type="text" class="form-control parsley-error" name="firstname" id="firstname" required="" data-parsley-group="block-0" data-parsley-id="24"> 
+ 
+                                        <textarea rows="3" class="form-control editor" name="content"></textarea> 
+ 
+									  </div> 
+										 
+									  <br><br> 
+										 
+									  <div class="form-navigation"> 
+										<button type="button" class="previous btn btn-info pull-left" style="display: none;">&lt; Previous</button> 
+										<button type="button" class="next btn btn-info pull-right">Ajouter l'article</button> 
+										<input type="submit" class="btn btn-primary pull-right" style="display: none;"> 
+										<span class="clearfix"></span> 
+									  </div> 
+ 
+									</form> 
+									 
+ 
+								 
+							</div>														 
+    			 
+														 
+						</div><!-- end card-->					 
+                    </div> 
+					 
+			</div> 
+ 
+ 
+ 
+<!-- BEGIN Java Script for this page --> 
+<script src="assets/plugins/trumbowyg/trumbowyg.min.js"></script> 
+<script> 
+$(document).ready(function () { 
+    'use strict'; 
+	$('.editor').trumbowyg();							 
+});  
+</script> 
+<!-- END Java Script for this page --> 
+ 
--- Project/Admin/table-article.php
+++ Project/Admin/table-article.php
@@ -103,9 +103,10 @@
 											</div>
 									</div>
 						</div>
-						<div class="row">
-							<button type="button" class="btn btn-success">New</button>
-						</div>
+						<div class="row" id="content">
+							<!-- c'était pas difficile bordel, tu as passé du temps a tout faire mais même pas fichu de faire ce lien !! !-->
+							<button class="btn btn-success" id="btn_add">New</button>
+						
                         <table class="table table-striped table-dark">
                         <table class="table table-striped">
                         <thead>
@@ -184,104 +185,21 @@
 				time: 600
 			});
 		} );		
+
+		$("#btn_add").on("click", function() {
+			$.ajax({
+				url: "ajax/addArticle.php",
+				type: "GET",
+				success: function(data) {
+					$("#content").html(data);
+				}
+				error : function(error) {
+					alert("Erreur");
+				}
+			})
+		})
 	</script>
 	
-	<script>
-	var ctx1 = document.getElementById("lineChart").getContext('2d');
-	var lineChart = new Chart(ctx1, {
-		type: 'bar',
-		data: {
-			labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-			datasets: [{
-					label: 'Dataset 1',
-					backgroundColor: '#3EB9DC',
-					data: [10, 14, 6, 7, 13, 9, 13, 16, 11, 8, 12, 9] 
-				}, {
-					label: 'Dataset 2',
-					backgroundColor: '#EBEFF3',
-					data: [12, 14, 6, 7, 13, 6, 13, 16, 10, 8, 11, 12]
-				}]
-				
-		},
-		options: {
-						tooltips: {
-							mode: 'index',
-							intersect: false
-						},
-						responsive: true,
-						scales: {
-							xAxes: [{
-								stacked: true,
-							}],
-							yAxes: [{
-								stacked: true
-							}]
-						}
-					}
-	});
-
-
-	var ctx2 = document.getElementById("pieChart").getContext('2d');
-	var pieChart = new Chart(ctx2, {
-		type: 'pie',
-		data: {
-				datasets: [{
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: [
-						'rgba(255,99,132,1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
-					],
-					label: 'Dataset 1'
-				}],
-				labels: [
-					"Red",
-					"Orange",
-					"Yellow",
-					"Green",
-					"Blue"
-				]
-			},
-			options: {
-				responsive: true
-			}
-	 
-	});
-
-
-	var ctx3 = document.getElementById("doughnutChart").getContext('2d');
-	var doughnutChart = new Chart(ctx3, {
-		type: 'doughnut',
-		data: {
-				datasets: [{
-					data: [12, 19, 3, 5, 2, 3],
-					backgroundColor: [
-						'rgba(255,99,132,1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
-					],
-					label: 'Dataset 1'
-				}],
-				labels: [
-					"Red",
-					"Orange",
-					"Yellow",
-					"Green",
-					"Blue"
-				]
-			},
-			options: {
-				responsive: true
-			}
-	 
-	});
-	</script>
 <!-- END Java Script for this page -->
 
 </body>
