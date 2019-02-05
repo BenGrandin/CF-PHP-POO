cd90bfb44c4d36f3bfe5fec002010f198b758504
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2019-02-05 09:53:16 +0100

    examples story 1

--- Project/index.php
+++ Project/index.php
@@ -74,12 +74,17 @@
                 <div class="row">
                     <div class="col-1"></div>
                     <div class="col-5 explanations">
-                        <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.</p>
+                        <p>De manière procédurale, on ne peut pas instancier d'objet,</p><p> ce qui ne permet pas de personification.</p>
                     </div>
                     <div class="col-1"></div>
                     <div class="col-4 examples">
-                        <pre><code>function lorem() {
-                            echo "salut"; }</code></pre>
+                        <pre><code>
+public function describe($name, $flag, $language)
+{
+$pays = $name + " " + $flag + " " + $language;
+return $pays;
+}
+                        </code></pre>
                     </div>
                     <div class="col-1"></div>
                 </div>
