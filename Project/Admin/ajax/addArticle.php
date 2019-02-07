5d41d493bf0c84c903f65b43c3e28398d0ff03b0
Doryan Lièvre <doryan.lievre@edu.itescia.fr>
2019-02-07 10:25:44 +0100

    english patch for BDD

--- Project/Admin/ajax/addArticle.php
+++ Project/Admin/ajax/addArticle.php
@@ -14,14 +14,14 @@ spl_autoload_register('my_autoloader');
 						<div class="card mb-3"> 
 							<div class="card-header"> 
 								<h3><i class="fa fa-file-o"></i> Add  article</h3> 
-								Et faire en sorte que ca marche. 
+								Write what you want
 							</div> 
 							<div class="card-body"> 
 								 
  
 								<form class="demo-form" action="#" novalidate=""> 
 									  <div class="form-section current"> 
-										<label for="firstname">Nom de l'article : </label> 
+										<label for="firstname">Name of the article: </label> 
 										<input type="text" class="form-control parsley-error" name="firstname" id="firstname" required="" data-parsley-group="block-0" data-parsley-id="24"> 
  
                                         <textarea rows="3" class="form-control editor" name="content"></textarea> 
@@ -32,7 +32,7 @@ spl_autoload_register('my_autoloader');
 										 
 									  <div class="form-navigation"> 
 										<button type="button" class="previous btn btn-info pull-left" style="display: none;">&lt; Previous</button> 
-										<button type="button" class="next btn btn-info pull-right">Ajouter l'article</button> 
+										<button type="button" class="next btn btn-info pull-right">Add article</button> 
 										<input type="submit" class="btn btn-primary pull-right" style="display: none;"> 
 										<span class="clearfix"></span> 
 									  </div> 
--- Project/Admin/ajax/addUser.php
+++ Project/Admin/ajax/addUser.php
@@ -8,21 +8,21 @@ spl_autoload_register('my_autoloader');
 
 						<div class="card mb-3">
 							<div class="card-header">
-								<h3><i class="fa fa-check-square-o"></i>Ajouter un utilisateur</h3>
-								Permet d'octroyer les accès à une personne.
+								<h3><i class="fa fa-check-square-o"></i>Add User</h3>
+								Allows granting access to a person.	
 							</div>
 								
 							<div class="card-body">
 								
 								<form autocomplete="off" action="#">
 								  <div class="form-group row">
-									<label for="inputEmail3" class="col-sm-2 col-form-label">Nom d'utilisateur</label>
+									<label for="inputEmail3" class="col-sm-2 col-form-label">User name</label>
 									<div class="col-sm-10">
 									  <input type="email" class="form-control" id="inputEmail3" placeholder="Email" autocomplete="off">
 									</div>
 								  </div>
 								  <div class="form-group row">
-									<label for="inputPassword3" class="col-sm-2 col-form-label">Mot de passe</label>
+									<label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
 									<div class="col-sm-10">
 									  <input type="password" class="form-control" id="inputPassword3" placeholder="Password" autocomplete="off">
 									</div>
